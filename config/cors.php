7c938666c32845cf4d60147d83d23fc2ff911966
Cmeng3 <134275497+Cmeng3@users.noreply.github.com>
2026-09-18 02:47:48 +0700

    Update allowed_origins in CORS configuration (add the domain frontend)

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    'allowed_origins' => array_filter(array_map('trim', explode(',', env('FRONTEND_URL', 'http://localhost:3000')))),
+    'allowed_origins' => array_filter(array_map('trim', explode(',', env('FRONTEND_URL', 'http://localhost:3000' , 'https://meng-portfolio-fzophdmch-meng-284f.vercel.app')))),
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['Accept', 'Content-Type', 'Authorization', 'X-XSRF-TOKEN', 'X-CSRF-TOKEN', 'X-Requested-With'],
     'exposed_headers' => [],
